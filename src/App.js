def1adf21bf288cb71cc59e9243b7c1a2ebc759f
jameslenahan <57712403+jameslenahan@users.noreply.github.com>
2020-08-19 12:14:23 -0400

    added current user tab

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
 import Home from './components/Home.js'
 import NavBar from './components/NavBar.js'
+import { getCurrentUser } from './actions/currentUsers.js'
 
-function App() {
-  return (
-      <Router>
-          {this.props.loggedin ? <NavBar loggedin={this.props.loggedin} /> : <Home />}
-    <div className="App">
-      <Switch>
-        <Route exact path='/' render={() => <Home loggedin={this.props.loggedin} />}/>
+class App extends Component {
+  componentDidMount(){
+    this.props.getCurrentUser()
+  }
+  render() {
+    return (
+        <Router>
+          {this.props.loggedin ? <NavBar loggedin={this.props.loggedin}/> : <Home/>}
+          <div className="App">
+            <Switch>
+              <Route exact path='/' render={() => <Home loggedin={this.props.loggedin}/>}/>
 
-      </Switch>
-    </div>
-      </Router>
-  );
+            </Switch>
+          </div>
+        </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/actions/currentUsers.js
@@ -0,0 +1,97 @@
+import { resetSignupForm } from './signupForm.js'
+import { resetLoginForm } from './loginForm.js'
+export const setCurrentUser = user => {
+    return {
+        type: 'SET_CURRENT_USER',
+        user
+    }
+}
+
+export const clearCurrentUser = () => {
+    return {
+        type: 'CLEAR_CURRENT_USER'
+    }
+}
+
+
+export const signup = (userData, history) => {
+    //console.log("action", userData)
+    const HEROKU_URL = ''
+    // process.env.REACT_APP_HEROKU
+    return dispatch => {
+        return fetch(`${HEROKU_URL}/api/v1/signup`, {
+            credentials: "include",
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({user: userData})
+        })
+            .then(resp => resp.json())
+            .then(user =>
+            {
+                if (user.error) {
+                    alert(user.error)
+                } else {
+                    dispatch(setCurrentUser(user))
+                    dispatch(resetSignupForm())
+                    history.push('/songs')
+                }
+
+            })
+    }
+}
+
+
+export const login = (userData, history) => {
+    const HEROKU_URL = ''
+    return dispatch => {
+        return fetch(`${HEROKU_URL}/api/v1/login`, {
+            credentials: 'include',
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(userData)
+        })
+            .then(resp => resp.json())
+            .then(user => {
+                if (user.error) {
+                    alert(user.error)
+                } else {
+                    dispatch(setCurrentUser(user))
+                    dispatch(resetLoginForm())
+                    history.push('/songs')
+                }
+            })
+    }
+}
+
+export const getCurrentUser = () => {
+    const HEROKU_URL = ''
+    return dispatch => {
+        return fetch(`${HEROKU_URL}/api/v1/get_current_user`, {
+            credentials: "include",
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            },
+        })
+            .then(resp => resp.json())
+            .then(user => {
+
+                dispatch(setCurrentUser(user))
+            })
+    }
+}
+
+export const logout = (event) => {
+    const HEROKU_URL = ''
+    return dispatch => {
+        dispatch(clearCurrentUser)
+        return fetch(`${HEROKU_URL}/api/v1/logout`, {
+            credentials: "include",
+            method: "DELETE"
+        })
+    }
+}
